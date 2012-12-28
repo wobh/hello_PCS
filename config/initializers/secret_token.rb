1b221bc26d056511c17151292a39f704b04efacb
William Clifford <wobh@yahoo.com>
2012-12-27 20:25:50 -0800

    Initial commit
    - ran "rails new"
    - ran "rails generate controller home index"
    - failed to create repository
    - renamed file "public/index.html" to "public/index-old.html"
    - customized file "app/views/home/index.html.erb"
    - created "root to => "home#index" in file "config/routes.rb"
    - ran "rails server" and tested site
    - created file "README.org"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'f152804b371a1fe698927af2a19c08169997a5ac0d8b551987e274f2d2d45d3be0e1ef7ea2c2ff0ecce6c9cebeb12287f164d2cf66b9aa112ef6b1dcddfa3ece'
